a8c9b786b0f862ee5ebea84fe2140be5f587a39e
Mu4a4o <3com@list.ru>
2021-05-31 10:22:43 +0300

    update

--- logic.py
+++ logic.py
@@ -1,8 +1,10 @@
 import pygame
-from tank_user import Tank_gissinits, Tank_base, Tank_tower
+from tank_user import Tank_gissinits, Tank_base, Tank_tower, Rocket
 
 pygame.init()
-sc = pygame.display.set_mode((1024, 768), pygame.DOUBLEBUF | pygame.HWSURFACE)
+W = 1024
+H = 768
+sc = pygame.display.set_mode((W, H), pygame.DOUBLEBUF | pygame.HWSURFACE)
 pygame.display.set_caption('poligonAI')
 сlock = pygame.time.Clock()
 RED = (255, 0, 0)
@@ -13,13 +15,15 @@ surf.fill(RED)
 tank_one = pygame.sprite.Group()
 tank_gissinits_left = Tank_gissinits(tank_x=400, tank_y=400, image_size=image_size)
 tank_gissinits_right = Tank_gissinits(tank_x=400, tank_y=400, right_true=True, image_size=image_size)
-tank_base = Tank_base(tank_gissinits_left.rect.topright[0], tank_gissinits_left.rect.topright[1], image_size=image_size)
-tank_tower = Tank_tower(tank_gissinits_left.rect.topright[0], tank_gissinits_left.rect.topright[1], image_size=image_size)
-tank_one.add(tank_gissinits_left, tank_gissinits_right, tank_base, tank_tower)
+tank_base = Tank_base(tank_x=400, tank_y=400, image_size=image_size)
+tank_tower = Tank_tower(tank_x=400, tank_y=400, image_size=image_size)
+rock = Rocket(tank_x=500, tank_y=500, image_size=image_size)
+rock = Rocket(tank_x=tank_tower.image.get_rect().top, tank_y=tank_tower.image.get_rect().bottom, image_size=image_size)
 
-
+tank_one.add(tank_gissinits_left, tank_gissinits_right, tank_base,tank_tower)
 running = True
 while running:
+
     for event in pygame.event.get():
         # действие при выходе
         if event.type == pygame.QUIT:
@@ -27,8 +31,11 @@ while running:
             running = False
     mouse = pygame.mouse.get_pressed()
     keys = pygame.key.get_pressed()
-    if mouse[0]:
-        tank_tower.shoots(fire=True)
+    if not rock.shot:
+        if mouse[0]:
+            tank_tower.shoots(fire=True)
+            rock.shoot()
+
     # управление танком и изменение его свойств
     if keys[pygame.K_UP] and not keys[pygame.K_RIGHT] and not keys[pygame.K_LEFT]:
         tank_gissinits_left.rotate(up=True)
@@ -62,11 +69,17 @@ while running:
         tank_base.rotate(right_down_rot=True)
         tank_tower.rotate(right_down_rot=True)
 
-    sc.fill((0, 0, 0))
+    rock.update(tank_x=tank_tower.tank_x, tank_y=tank_tower.tank_y)
+    sc.blit(rock.image, rock.rect)
     tank_one.draw(sc)
     tank_one.update()
-    pygame.display.update()
 
+
+
+
+    # rock.update(tank_x=tank_tower.tank_x, tank_y=tank_tower.tank_y)
+    pygame.display.update()
+    sc.fill((0, 0, 0))
     # FPS 60
 
     сlock.tick(60)
--- tank_user.py
+++ tank_user.py
@@ -1,6 +1,7 @@
 import math
 import pygame
-'test'
+import time
+
 
 class Tank_gissinits(pygame.sprite.Sprite):
     def __init__(self, tank_x, tank_y, right_true=False, image_size=5):
@@ -24,7 +25,7 @@ class Tank_gissinits(pygame.sprite.Sprite):
         self.image = pygame.transform.scale(self.image_group[0], (self.image_group[0].get_width() // self.image_size,
                                                                   self.image_group[0].get_height() // self.image_size))
         self.image = pygame.transform.rotate(self.image, 0)
-        self.rect = self.image.get_rect(topright=(self.tank_x, self.tank_y))
+        self.rect = self.image.get_rect(center=(self.tank_x, self.tank_y))
 
     @property
     def gissinits_count(self):
@@ -44,8 +45,7 @@ class Tank_gissinits(pygame.sprite.Sprite):
                                             (self.image_group[self.gissinits_count].get_width() // self.image_size,
                                              self.image_group[self.gissinits_count].get_height() // self.image_size))
         self.image = pygame.transform.rotate(self.image, self.angle)
-        self.rect = self.image.get_rect(topright=(self.tank_x, self.tank_y))
-        self.rect.center = round(self.tank_x), round(self.tank_y)
+        self.rect = self.image.get_rect(center=(self.tank_x, self.tank_y))
 
     def rotate(self, up=False, down=False, left_up_rot=False,
                right_up_rot=False, left_down_rot=False, right_down_rot=False):
@@ -125,11 +125,10 @@ class Tank_base(pygame.sprite.Sprite):
         self.image = pygame.transform.scale(self.path_image, (self.path_image.get_width() // self.image_size,
                                                               self.path_image.get_height() // self.image_size))
         self.image = pygame.transform.rotate(self.image, 0)
-        self.rect = self.image.get_rect(topleft=(self.tank_x, self.tank_y))
+        self.rect = self.image.get_rect(center=(self.tank_x, self.tank_y))
 
     def update(self):
-        self.rect = self.image.get_rect(topleft=(self.tank_x, self.tank_y))
-        self.rect.center = round(self.tank_x), round(self.tank_y)
+        self.rect = self.image.get_rect(center=(self.tank_x, self.tank_y))
 
     def rotate(self, up=False, down=False, left_up_rot=False,
                right_up_rot=False, left_down_rot=False, right_down_rot=False):
@@ -194,6 +193,7 @@ class Tank_tower(pygame.sprite.Sprite):
         self.tank_rotate_angle = 90
         self.image_size = image_size
         self.shoots_count = 0
+        self.shoots_first = time.time()
         self.image_group_shoots = [pygame.image.load('sprite/tank tower/shoot/0.png').convert_alpha(),
                                    pygame.image.load('sprite/tank tower/shoot/1.png').convert_alpha(),
                                    pygame.image.load('sprite/tank tower/shoot/2.png').convert_alpha(),
@@ -211,11 +211,13 @@ class Tank_tower(pygame.sprite.Sprite):
         self.image = pygame.transform.rotate(self.image, self.angle)
         self.rect = self.image.get_rect(center=(self.tank_x, self.tank_y))
 
-    def shoots(self, fire = False):
-        if 0 < self.shoots_count <10:
+    def shoots(self, fire=False):
+        shoots_first = time.time() - self.shoots_first
+        if 0 < self.shoots_count < 10:
             self.shoots_count += 1
-        elif self.shoots_count >= 10 or self.shoots_count == 0:
+        elif self.shoots_count == 10 and shoots_first >= 2:
             self.shoots_count = 0
+            self.shoots_first = time.time()
         if fire and self.shoots_count == 0:
             self.shoots_count += 1
 
@@ -258,5 +260,99 @@ class Tank_tower(pygame.sprite.Sprite):
                                                  self.shoots_count].get_height() // self.image_size))
         self.image = pygame.transform.rotate(self.image, self.angle)
         self.rect = self.image.get_rect(center=(self.tank_x, self.tank_y))
+
+
+
+class Rocket(pygame.sprite.Sprite):
+    def __init__(self, tank_y, tank_x, image_size=5):
+        pygame.sprite.Sprite.__init__(self)
+        self.tank_y = tank_y
+        self.tank_x = tank_x
+        self.tank_y_first = tank_y
+        self.tank_x_first = tank_x
+        self.angle = 90
+        self.tank_rotate_angle = 90
+        self.image_size = image_size
+        self.shoots_count = 0
+        self.shoots_first = time.time()
+        self.shot = False
+        self.mouse_x_shoot = 0
+        self.mouse_y_shoot = 0
+        self.bresenham_line = []
+        self.path_image = pygame.image.load('sprite/rocket/0.png').convert_alpha()
+        self.image = pygame.transform.scale(self.path_image, (self.path_image.get_width() // self.image_size,
+                                                              self.path_image.get_height() // self.image_size))
+        self.rect = self.image.get_rect(center=(self.tank_x, self.tank_y))
+
+    def bresenham(self,x1=0, y1=0, x2=0, y2=0, speed = 1):
+
+        dx = x2 - x1
+        dy = y2 - y1
+
+        sign_x = 1 if dx > 0 else -1 if dx < 0 else 0
+        sign_y = 1 if dy > 0 else -1 if dy < 0 else 0
+
+        if dx < 0: dx = -dx
+        if dy < 0: dy = -dy
+
+        if dx > dy:
+            pdx, pdy = sign_x, 0
+            es, el = dy, dx
+        else:
+            pdx, pdy = 0, sign_y
+            es, el = dx, dy
+
+        x, y = x1, y1
+
+        error, t = el / 2, 0
+
+        list_line_all = []
+        while t < el:
+            error -= es
+            if error < 0:
+                error += el
+                x += sign_x
+                y += sign_y
+            else:
+                x += pdx
+                y += pdy
+            t += 1
+            list_line_all.append((x, y))
+        list_line_speed = []
+        for i in range(0,len(list_line_all), speed):
+            list_line_speed.append(list_line_all[i])
+        return iter(list_line_speed)
+
+    def shoot(self):
+        self.shot = True
+        self.mouse_x_shoot, self.mouse_y_shoot = pygame.mouse.get_pos()
+        self.bresenham_line = self.bresenham(self.tank_x, self.tank_y, self.mouse_x_shoot, self.mouse_y_shoot, speed= 7)
+
+
+    def update(self, tank_x, tank_y):
+        if self.shot:
+            try:
+                self.tank_x, self.tank_y = next(self.bresenham_line)
+            except StopIteration:
+                self.tank_x = tank_x
+                self.tank_y = tank_y
+                self.shot = False
+            self.angle = math.degrees(math.atan2((self.tank_x - self.mouse_x_shoot), (self.tank_y - self.mouse_y_shoot)))
+            self.image = pygame.transform.scale(self.path_image, (self.path_image.get_width() // self.image_size,
+                                                                  self.path_image.get_height() // self.image_size))
+            self.image = pygame.transform.rotate(self.image, self.angle)
+            self.rect = self.image.get_rect(center=(self.tank_x, self.tank_y))
+
+
+        else:
+            self.tank_y = tank_y
+            self.tank_x = tank_x
+            self.mouse_x, self.mouse_y = pygame.mouse.get_pos()
+            self.angle = math.degrees(math.atan2((self.tank_x - self.mouse_x), (self.tank_y - self.mouse_y)))
+            self.image = pygame.transform.scale(self.path_image, (self.path_image.get_width() // self.image_size,
+                                                                  self.path_image.get_height() // self.image_size))
+            self.image = pygame.transform.rotate(self.image, self.angle)
+            self.rect = self.image.get_rect(center=(self.tank_x, self.tank_y))
+
 
 
