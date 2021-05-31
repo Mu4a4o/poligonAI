c6e774f26182fa5a5abd6053617e1391262960c5
Mu4a4o <3com@list.ru>
2021-05-31 10:22:43 +0300

    update

---
+++ test.py
@@ -0,0 +1,45 @@
+def bresenham(x1=0, y1=0, x2=0, y2=0, speed = 0):
+    dx = x2 - x1
+    dy = y2 - y1
+
+    sign_x = 1 if dx > 0 else -1 if dx < 0 else 0
+    sign_y = 1 if dy > 0 else -1 if dy < 0 else 0
+
+    if dx < 0: dx = -dx
+    if dy < 0: dy = -dy
+
+    if dx > dy:
+        pdx, pdy = sign_x, 0
+        es, el = dy, dx
+    else:
+        pdx, pdy = 0, sign_y
+        es, el = dx, dy
+
+    x, y = x1, y1
+
+    error, t = el / 2, 0
+
+    list_line = []
+    while t < el:
+        error -= es
+        if error < 0:
+            error += el
+            x += sign_x
+            y += sign_y
+        else:
+            x += pdx
+            y += pdy
+        t += 1
+        list_line.append((x,y))
+    if speed != 1:
+        for i in range(0,len(list_line), speed):
+            del list_line[i:i+speed]
+    return  list_line
+    #return iter(list_line)
+a =  bresenham(x1=0, y1=0, x2=10, y2=10, speed = 3)
+print(a)
+
+# x, y = next(a)
+# print(x,y)
+# x, y = next(a)
+# print(x,y)
